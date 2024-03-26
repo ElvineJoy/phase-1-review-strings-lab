0101de03b9ea6422c6ee6154f31294aa70d32576
ElvineJoy <elvinkuiyungest@gmail.com>
2024-03-26 13:39:38 +0300

    String Interpolation and concatenation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// second variable
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser} !`;
+
+// third variable
+ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//  fourth variable
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`;
